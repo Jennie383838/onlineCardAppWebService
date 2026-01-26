4938441906a0e4e25e0b99d39f2a5aa70f847148
Xavier Thong <xavierthong4@gmail.com>
2026-01-26 11:16:40 +0800

    new stuff

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 
 const allowedOrigins = [
     "http://localhost:3000",
-    "https://card-app-starter-z9o9-hemyf1rqh-xavier-thongs-projects.vercel.app",
+    "https://card-app-starter-z9o9.vercel.app/",
     "https://onlinecardappwebservice-iu6e.onrender.com",
 ];
 
